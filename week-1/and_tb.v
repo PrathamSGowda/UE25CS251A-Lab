d982d4fbd50c8726ba4a6b8efa33359c8ffb91e9
Pratham S Gowda <prathampsg2007@gmail.com>
2026-08-07 23:53:43 +0530

    testbench file

---
+++ week-1/and_tb.v
@@ -0,0 +1,28 @@
+// test bench
+
+module and_tb;
+    wire y;
+    reg a,b;
+    and1 a1(y,a,b);
+
+    initial
+    begin
+        #000 a=0;b=0;
+        #100 a=0;b=1;
+        #100 a=1;b=0;
+        #100 a=1;b=1;
+        #100 a=0;b=0;
+    end
+
+    initial
+    begin
+        $monitor($time,"a = %b, b = %b, y = %b",a,b,y);
+    end
+
+    initial
+    begin
+        $dumpfile("and.vcd");
+        $dumpvars(0,and_tb);
+    end
+
+endmodule
